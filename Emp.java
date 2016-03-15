176ce58215945a16b59845625306818276a9319d
failureone <bdqn_dc@163.com>
2016-03-15 09:52:15 +0800

    new

---
+++ Emp.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Emp implements Serializable{
+	
+	 private Integer empno;
+	 private String ename;
+	 private Date hiredate;
+	 private Double comm;
+	 private Double salary;
+	
+}
