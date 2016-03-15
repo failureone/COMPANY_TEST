2d87c95270210b077c2e31688df940cde28828ca
failureone <bdqn_dc@163.com>
2016-03-15 11:24:50 +0800

    Add Hello.java File

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+	  
+	  public static void main(String [] args){
+
+		   System.out.println("www.yootk.com");
+
+		   System.out.println("Hello World");
+
+		   System.out.println("############");
+	  }
+}
